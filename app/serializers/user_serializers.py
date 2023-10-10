879f171a3053c117ea77ed3ed41712d8f5217404
javierpb275 <javierpb275@gmail.com>
2023-10-10 19:48:50 +0200

    created user schemas and user serializers

---
+++ app/serializers/user_serializers.py
@@ -0,0 +1,38 @@
+def user_entity(user) -> dict:
+    return {
+        "id": str(user["_id"]),
+        "name": user["name"],
+        "email": user["email"],
+        "role": user["role"],
+        "photo": user["photo"],
+        "verified": user["verified"],
+        "password": user["password"],
+        "created_at": user["created_at"],
+        "updated_at": user["updated_at"]
+    }
+
+
+def user_response_entity(user) -> dict:
+    return {
+        "id": str(user["_id"]),
+        "name": user["name"],
+        "email": user["email"],
+        "role": user["role"],
+        "photo": user["photo"],
+        "created_at": user["created_at"],
+        "updated_at": user["updated_at"]
+    }
+
+
+def embedded_user_response(user) -> dict:
+    return {
+        "id": str(user["_id"]),
+        "name": user["name"],
+        "email": user["email"],
+        "photo": user["photo"]
+    }
+
+
+def user_list_entity(users) -> list:
+    return [user_entity(user) for user in users]
+
